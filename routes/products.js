91095db4f956c14c0178600e0d79388075fc804d
Perdly Setiawan <setiawan.perdly07@gmail.com>
2022-06-17 17:02:50 +0700

    fix insert product & add update and delete product endpoint

--- routes/products.js
+++ routes/products.js
@@ -1,36 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const productController = require('../controllers/products');
-const { products } = require('../models');
+const productController = require("../controllers/products");
 
-/* GET users listing. */
-router.get('/', async function(req, res) {
-    const products = await productController.findAll();
-    res.send({
-        data:products
-    });
-});
+router.get("/", productController.findAll);
 
-router.get('/:id', async function(req, res) {
-    const products = await productController.findById(req.params.id);
-    
-    res.send({
-        data:products
-    });
-});
+router.get("/:id", productController.findById);
 
-router.post('/', async function(req, res) {
-    const product = {
-        product_name: req.body.product_name,
-        product_price: req.body.product_price,
-        product_description: req.body.product_description,
-        product_image: req.body.product_image,
-        category_id: req.body.category_id,
-        user_id:req.body.user_id
-    };
-    const products = await productController.insert(product);
-    
-    res.json(products);
-});
+router.post("/", productController.insert);
+
+router.put("/:id", productController.update);
+
+router.delete("/:id", productController.destroy);
 
 module.exports = router;

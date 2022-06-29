ef1ace107d98b672e57bf24264fe61ea4ed95f52
Perdly Setiawan <setiawan.perdly07@gmail.com>
2022-06-29 20:43:13 +0700

    try modify config.js to use .env

--- config/config.js
+++ config/config.js
@@ -19,10 +19,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: `${process.env.DB_USERNAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    host: `${process.env.DB_HOST}`,
     dialect: "postgres",
     dialectOptions: {
       ssl: {

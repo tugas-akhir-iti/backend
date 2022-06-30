c580b5b967ff0a7d14db8cb8247f898186e720aa
Perdly Setiawan <setiawan.perdly07@gmail.com>
2022-06-30 21:49:06 +0700

    add middleware for check is user login and check is user profile complete

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const userController = require("../controllers/user");
+const middlewareVerifyToken = require("../middlewares/verifyToken");
 
-router.get("/:id", userController.get);
+router.get("/:id", middlewareVerifyToken, userController.get);
 router.post("/", userController.create);
-router.put("/:id", userController.update);
+router.put("/:id", middlewareVerifyToken, userController.update);
 router.post("/login", userController.login);
 
 module.exports = router;

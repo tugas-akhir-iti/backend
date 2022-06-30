cd9aa98206765dbff9771034a5080c54e39982fc
Perdly Setiawan <setiawan.perdly07@gmail.com>
2022-06-30 16:40:04 +0700

    add update status order

--- controllers/order.js
+++ controllers/order.js
@@ -12,6 +12,22 @@ async function insert(req, res) {
   res.json(createOrder);
 }
 
+async function update(req, res) {
+  const checkIfOrderExist = await Orders.findByPk(req.params.id);
+  if (checkIfOrderExist) {
+    const order = {
+      order_status: req.fields.order_status,
+    };
+    await Orders.update(order, {
+      where: { id: req.params.id },
+    });
+    res.send({ message: "Data order berhasil diupdate" });
+  } else {
+    res.send({ message: "Order tersebut tidak ada" });
+  }
+}
+
 module.exports = {
   insert,
+  update,
 };

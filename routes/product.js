6d3b3baa15aaba146a6e0ef012cfc44dccf8e01f
Perdly Setiawan <setiawan.perdly07@gmail.com>
2022-07-15 21:10:50 +0700

    add cors

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
+const cors = require("cors");
 const productController = require("../controllers/product");
 const middlewareVerifyToken = require("../middlewares/verifyToken");
 const middlewareProfileIsComplete = require("../middlewares/profileIsComplete");
+
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 router.get("/", productController.findAll);
 
@@ -10,12 +18,32 @@ router.get("/:id", productController.findById);
 
 router.get("/categories/:id", productController.findByCategoryId);
 
-router.post("/", middlewareVerifyToken, middlewareProfileIsComplete, productController.insert);
+router.post(
+  "/",
+  middlewareVerifyToken,
+  middlewareProfileIsComplete,
+  productController.insert
+);
 
-router.put("/:id", middlewareVerifyToken, middlewareProfileIsComplete, productController.update);
+router.put(
+  "/:id",
+  middlewareVerifyToken,
+  middlewareProfileIsComplete,
+  productController.update
+);
 
-router.put("/status/:id", middlewareVerifyToken, middlewareProfileIsComplete, productController.updateStatusProduct);
+router.put(
+  "/status/:id",
+  middlewareVerifyToken,
+  middlewareProfileIsComplete,
+  productController.updateStatusProduct
+);
 
-router.delete("/:id", middlewareVerifyToken, middlewareProfileIsComplete, productController.destroy);
+router.delete(
+  "/:id",
+  middlewareVerifyToken,
+  middlewareProfileIsComplete,
+  productController.destroy
+);
 
 module.exports = router;
